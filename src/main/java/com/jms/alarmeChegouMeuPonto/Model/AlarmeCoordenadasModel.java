0cd1053eb80433cca0d5ba064f08ba1c03dcaf67
Jonas Magalhães Silva <80786958+jonasmagalhaes-8@users.noreply.github.com>
2023-11-19 22:51:42 -0300

    FIX

--- src/main/java/com/jms/alarmeChegouMeuPonto/Model/AlarmeCoordenadasModel.java
+++ src/main/java/com/jms/alarmeChegouMeuPonto/Model/AlarmeCoordenadasModel.java
@@ -2,13 +2,15 @@ package com.jms.alarmeChegouMeuPonto.Model;
 
 import java.io.Serializable;
 import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_coordenadas_alarme")
